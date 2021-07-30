d333601ffac03ccb8d42d22f485a90d014c2de92
26seung <yousi7@naver.com>
2021-07-30 18:51:09 +0900

    web-chat

--- src/main/java/com/one/shade/repository/CastingRepository.java
+++
@@ -1,7 +0,0 @@
-package com.one.shade.repository;
-
-import com.one.shade.domain.Casting;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CastingRepository extends JpaRepository<Casting,Long> {
-}
--- src/main/java/com/one/shade/repository/EpisodeRepository.java
+++
@@ -1,7 +0,0 @@
-package com.one.shade.repository;
-
-import com.one.shade.domain.Episode;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface EpisodeRepository extends JpaRepository<Episode,Long> {
-}
